6e85f6736589362d6629ebeda9b8a0b6761a6165
dushyant264 <ddushyant89@gmail.com>
2024-01-03 01:58:51 +0530

    gg

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -42,8 +42,8 @@ const Login = () => {
 
   return (
     <div>
-      <section className='pl-[10rem] flex flex-wrap '>
-        <div className='mr-[4rem] mt-[5rem]'>
+      <section className='pl-[10rem] flex   overflow-hidden  '>
+        <div className='mr-[4rem] mt-[5rem] '>
           <h1 className="text-2xl font-semibold mb-4">Sign In</h1>
 
           <form onSubmit={submitHandler} className='container w-[40rem]'>
@@ -81,6 +81,12 @@ const Login = () => {
             </p>
           </div>
         </div>
+
+        <img
+          src="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80"
+          alt=""
+          className="h-[90vh] w-[59%] xl:flex md:hidden sm:hidden rounded-lg justify-center align-middle mt-[2rem] "
+        />
       </section>
     </div>
   )
